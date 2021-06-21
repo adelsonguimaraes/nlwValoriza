1820463184f8f0ce33b7c38d2057a9ddd2203ee4
Adelson Guimarães Monteiro <adelsonguimaraes@gmail.com>
2021-06-20 21:01:51 -0400

    iniciando

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express from "express";
+
+const app = express();
+/*
+* GET       => Buscar uma informação
+* POST      => Inserir (Criar) uma informação
+* PUT       => Alterar uma informação
+* DELETE    => Remover um dado
+* PATCH     => Alterar uma informação espercífica
+*/
+
+app.get('/test', (request, response) => {
+    // request tudo que está entrando
+    // response tudo que está saindo
+    return response.send("Olá NLW");
+});
+
+app.post('/test-post', (request, response) => {
+    return response.send("Olá NLW método POST");
+});
+
+// http://localhost:3000
+app.listen(3000, () => console.log("Server is running"));
