d661186cd292141e2dc99f4e29f9aaf32d938394
Adelson Guimarães Monteiro <adelsonguimaraes@gmail.com>
2021-07-02 18:01:12 -0400

    atualizando

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,8 @@
+import "reflect-metadata";
 import express from "express";
 
+import "./database";
+
 const app = express();
-/*
-* GET       => Buscar uma informação
-* POST      => Inserir (Criar) uma informação
-* PUT       => Alterar uma informação
-* DELETE    => Remover um dado
-* PATCH     => Alterar uma informação espercífica
-*/
 
-app.get('/test', (request, response) => {
-    // request tudo que está entrando
-    // response tudo que está saindo
-    return response.send("Olá NLW");
-});
-
-app.post('/test-post', (request, response) => {
-    return response.send("Olá NLW método POST");
-});
-
-// http://localhost:3000
 app.listen(3000, () => console.log("Server is running"));
